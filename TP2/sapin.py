8b9b6c064e82298294cb78afcdaaed8d53a54595
Waterlo admin <contact@waterlo-agency.com>
2023-01-26 14:32:05 +0100

    TP2-1.1

--- TP2/sapin.py
+++ TP2/sapin.py
@@ -1,6 +1,6 @@
 #Question 1.1;
 def a_la_chaine(n):
-    """Retourne une chaîne de caractères représentant un sapin de hauteur n"""
+    """Retourne une chaîne de caractères composé d'une juxtaposition de n caractères A"""
     sapin = ""
     for i in range(n):
         sapin = "A" * (n)
@@ -9,3 +9,7 @@ def a_la_chaine(n):
 hello = a_la_chaine(5)
 print(hello)
 print(len(hello))
+
+#Question 1.2;
+def a_la_chaine2(esp,n):
+    """Retourne une chaîne de caractères composé d'espaces suivis de n caractères A"""
