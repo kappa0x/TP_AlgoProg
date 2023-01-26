4eb23cbb097a26103d5fceb863e89191702f28be
Waterlo admin <contact@waterlo-agency.com>
2023-01-26 14:47:00 +0100

    TP2 - question 1.2

--- TP2/sapin.py
+++ TP2/sapin.py
@@ -6,10 +6,14 @@ def a_la_chaine(n):
         sapin = "A" * (n)
     return sapin
 
-hello = a_la_chaine(5)
-print(hello)
-print(len(hello))
-
 #Question 1.2;
 def a_la_chaine2(esp,n):
     """Retourne une chaîne de caractères composé d'espaces suivis de n caractères A"""
+    sapin = ""
+    for i in range(n):
+        sapin = " " * (esp) + "A" * (n)
+    return sapin
+
+hello = a_la_chaine2(2,7)
+print(hello)
+print(len(hello))
